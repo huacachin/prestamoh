a9838916220880b9b37a7cdb1d5781f0bac8c509
Antony Culqui <a.culqui02@gmail.com>
2026-06-05 15:04:19 -0500

    fix(clientes): mas ancho en columnas Fecha, Usuario, Exp., Asesor y Opciones (min-width)

--- resources/views/livewire/clients/index.blade.php
+++ resources/views/livewire/clients/index.blade.php
@@ -257,5 +257,13 @@
     .clients-legacy th.col-wrap, .clients-legacy td.col-wrap {
         white-space: normal; min-width: 180px; max-width: 300px;
     }
+    /* Un poco más de ancho en columnas puntuales (Fecha, Usuario, Exp., Asesor, Opciones). */
+    .clients-legacy tbody td:nth-child(2)  { min-width: 90px; }  /* Fecha */
+    .clients-legacy tbody td:nth-child(3)  { min-width: 78px; }  /* Usuario */
+    .clients-legacy tbody td:nth-child(4)  { min-width: 58px; }  /* Exp. */
+    .clients-legacy tbody td:nth-child(10) { min-width: 82px; }  /* Asesor */
+    .clients-legacy tbody td:nth-child(11),
+    .clients-legacy tbody td:nth-child(12),
+    .clients-legacy tbody td:nth-child(13) { min-width: 74px; }  /* Opciones (botones) */
 </style>
 </div>
