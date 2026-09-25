041c443c8e7fd3f8576499f5040d852ee9236d30
Antony Culqui <a.culqui02@gmail.com>
2026-09-25 11:41:37 -0500

    style(creditos): el título del cronograma lleva el número del crédito
    "CRONOGRAMA DE PAGOS #28822": al imprimir o compartir la pantalla se sabe
    de qué crédito es.

--- resources/views/livewire/credits/schedule.blade.php
+++ resources/views/livewire/credits/schedule.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-sm-6">
-            <h4 class="main-title title-modules" style="color:red;">CRONOGRAMA DE PAGOS</h4>
+            <h4 class="main-title title-modules" style="color:red;">CRONOGRAMA DE PAGOS #{{ $credit->id }}</h4>
         </div>
         <div class="col-sm-6 mt-sm-2">
             <ul class="breadcrumb breadcrumb-start float-sm-end">
